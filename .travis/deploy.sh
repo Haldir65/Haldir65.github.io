a2e798830f2af24cd231a47d8f921cf27a7c86c2
Haldir65 <mjw090608@gmail.com>
2018-01-22 13:20:58 +0800

    add commit Scripts

---
+++ .travis/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ev
+export TZ='Asia/Shanghai'
+
+# 先 clone 再 commit，避免直接 force commit
+git clone -b master git@github.com:Haldir65/Haldir65.github.io.git .deploy_git
+
+cd .deploy_git
+git checkout master
+mv .git/ ../public/
+cd ../public
+
+git add .
+git commit -m "Site auto updated: `date +"%Y-%m-%d %H:%M:%S"`"
+
+# # 同时 push 一份到自己的服务器上
+# git remote add vps git@prinzeugen.net:hexo.git
+#
+# git push vps master:master --force --quiet
+# git push origin master:master --force --quiet
