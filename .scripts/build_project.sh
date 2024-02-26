22975e99aeb6afa896efec35d4df6496cc9f80b6
Haldir65 <mjw090608@gmail.com>
2024-02-26 10:12:04 +0800

    [Build] enable actions

---
+++ .scripts/build_project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+echo "show current info"
+
+node --version
+npm --version
+
+apk add --update nodejs nodejs-npm
+
+rm -rf node_modules && npm install --force
+npm install -g hexo-cli@3.1.0
+
+hexo version
+hexo clean
+hexo generate
+
+tree -L 4
